cfe197f60f7728d4c3b9409bd19b83a422a217ab
rashi yadav <shashi7879728597@gmail.com>
2025-11-04 23:55:24 +0530

    fixed and rewrite the code and added jupyter file

---
+++ README.md
@@ -0,0 +1,2 @@
+this is my first repository and also my first task from aims 
+author - rashi yadav 
--- ordinal-encoding/hello.py
+++ ordinal-encoding/hello.py
@@ -3,16 +3,13 @@ import pandas as pd
 d = {'sales': [100000,222000,1000000,522000,111111,222222,1111111,20000,75000,90000,1000000,10000], 
      'city': ['Tampa','Tampa','Orlando','Jacksonville','Miami','Jacksonville','Miami','Miami','Orlando','Orlando','Orlando','Orlando'], 
      'size': ['Small', 'Medium','Large','Large','Small','Medium','Large','Small','Medium','Medium','Medium','Small']}
-df=pd.DataFrame(d)
-def encode_dataframe(df):
-     for key in df:
-           if type(df[key][0]) == str:
-             unique = list(set(df[key]))
-             df[key] = [unique.index(v) for v in df[key]]
-     return df
-encoded_df = encode_dataframe(df)
 
-for i in range(len(encoded_df)):
-    print(*[encoded_df[key][i] for key in encoded_df])
+for key in d:
+    if type(d[key][0]) == str:
+        unique = list(set(d[key]))
+        d[key] = [unique.index(v) for v in d[key]]
 
-print("Total rows:", len(encoded_df))
+for i in range(len(d['sales'])):
+    print(*[d[key][i] for key in d])
+
+print("Total rows:", len(d['sales']))
